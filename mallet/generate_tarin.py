2117d52648694b91c08df23a9a13d0397103e0f9
fatemeakbari <fatemeakbari76.oh@gmail.com>
2018-06-11 23:32:37 +0430

    Add files via upload

---
+++ mallet/generate_tarin.py
@@ -0,0 +1,24 @@
+# - *- coding: utf- 8 - *-
+f1 = open('rewriteActor.txt','r')
+f2 = open('rewriteSoccer.txt','r')
+f3 = open('train.txt','w')
+
+flag = True
+for i in range(0,10000):
+    cnt1 = 0;
+    cnt2 = 0;
+    
+    for line in f1:
+        if(cnt1 == 0):
+            f3.write(line)
+            
+            cnt1 +=1
+        if( cnt1 == 1):
+            break
+       
+    for line in f2:
+        if(cnt2 == 0):
+            f3.write(line)
+            cnt2 +=1
+        if( cnt2 == 1):
+            break
